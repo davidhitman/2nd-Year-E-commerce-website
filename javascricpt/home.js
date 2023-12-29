0af2fb9e6ef76e73e9748b0ffa8dc0f032cc8ff5
davidhitman <107502959+davidhitman@users.noreply.github.com>
2023-12-29 22:11:18 +0000

    Add files via upload

---
+++ javascricpt/home.js
@@ -0,0 +1,6 @@
+// the fuction that is called when the button "shop now "
+// in the home page is clicked
+// this function calls the page product.html
+function shop(){
+    window.location.href = "../php/product.php";
+}
